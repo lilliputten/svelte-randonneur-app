302a4fd584db21e5714288f9e5deb0deb8315108
lilliputten <lilliputten@yandex.ru>
2023-12-23 22:49:34 +0700

    Added root alias (`@/src`), moved current sample code into included page (`src/pages/Main/Main.svelte`).

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-	import { hasData, getHasData } from '$lib/store/hasData';
+	import Main from '@/src/pages/Main';
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<p><button on:click={() => hasData.update((value) => !value)}>Toggle data: {$hasData}</button></p>
+<Main />
