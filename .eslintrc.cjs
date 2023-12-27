86692e229f4cf48dac6e2e7408f74cb98cf1ed68
lilliputten <lilliputten@yandex.ru>
2023-12-27 15:36:06 +0700

    Updated project configuration: eslint (updated rules, prettier support), changed tab spacings to spaces.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,27 +5,45 @@ module.exports = {
 		'eslint:recommended',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:svelte/recommended',
-		'prettier'
+		'plugin:prettier/recommended',
+		// 'prettier',
 	],
+	rules: {
+		'no-console': 'warn',
+		'no-debugger': 'warn',
+		'@typescript-eslint/no-unused-vars': [
+			'warn',
+			{
+				varsIgnorePattern: '^_',
+				argsIgnorePattern: '^_',
+			},
+		],
+		'prettier/prettier': [
+			'warn',
+			{
+				endOfLine: 'auto',
+			},
+		],
+	},
 	parser: '@typescript-eslint/parser',
 	plugins: ['@typescript-eslint'],
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020,
-		extraFileExtensions: ['.svelte']
+		extraFileExtensions: ['.svelte'],
 	},
 	env: {
 		browser: true,
 		es2017: true,
-		node: true
+		node: true,
 	},
 	overrides: [
 		{
 			files: ['*.svelte'],
 			parser: 'svelte-eslint-parser',
 			parserOptions: {
-				parser: '@typescript-eslint/parser'
-			}
-		}
-	]
+				parser: '@typescript-eslint/parser',
+			},
+		},
+	],
 };
