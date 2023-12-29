1444ea4d6f12551ee8e4f874e061fb263815f881
lilliputten <lilliputten@yandex.ru>
2023-12-30 00:01:09 +0700

    Issue #3: Added DemoTable component.

--- src/components/RandoEditor/DataEditorWrapper/DataEditorWrapper.svelte
+++ src/components/RandoEditor/DataEditorWrapper/DataEditorWrapper.svelte
@@ -1,23 +1,17 @@
 <script lang="ts">
-  // import { browser } from '$app/environment';
-  // import { goto } from '$app/navigation';
-  // import { onDestroy, onMount } from 'svelte';
-  //
-  // import { addToast } from '@/src/components/ui/Toasts';
-  // import { hasDataStore, toggleHasData } from '@/src/store';
+  import DemoTable from './DemoTable';
 </script>
-
-<svelte:head>
-  <title>Edit data</title>
-</svelte:head>
 
 <div class="DataEditorWrapper">
   <div class="header">header</div>
   <div class="container scrollable">
     <div class="content">
+      <DemoTable />
+      <!--
       {#each Array(25) as _, idx}
         <p>{idx + 1}</p>
       {/each}
+      -->
     </div>
   </div>
 </div>
