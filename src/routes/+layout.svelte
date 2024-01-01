643f4199237d9d82df9c9805835b0ab8ace02b42
lilliputten <lilliputten@yandex.ru>
2024-01-02 00:18:02 +0700

    Issue #3: Minor changes: removed unused code, jest tests, .

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,35 +1,5 @@
 <script lang="ts">
-  import {
-    AppShell,
-    Aside,
-    Divider,
-    Footer,
-    Header,
-    Navbar,
-    ShellSection,
-    SvelteUIProvider,
-    Title,
-    colorScheme,
-    fns,
-  } from '@svelteuidev/core';
-
-  import Toasts from '@/src/components/ui/Toasts';
-
   import AppLayout from '@/src/components/app/AppLayout';
-
-  // import Header from '@/src/components/layout/Header';
-
-  let opened = false;
-
-  function toggleTheme() {
-    colorScheme.update((v) => (v === 'light' ? 'dark' : 'light'));
-  }
-  function toggleOpened() {
-    opened = !opened;
-  }
-
-  // import './styles.scss';
-  // import './styles-fixes.scss';
 </script>
 
 <AppLayout>
