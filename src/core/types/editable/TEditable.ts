d24e516f6214729ae77fa10d44eac2670feb35c7
lilliputten <lilliputten@yandex.ru>
2024-01-02 14:26:09 +0700

    Issue #3: Added EditableField -- basic editing component.

--- src/core/types/editable/TEditable.ts
+++ src/core/types/editable/TEditable.ts
@@ -1,8 +1,8 @@
 export const editableValueTypes = [
   // prettier-ignore
   'string',
-  'boolean',
   'number',
+  'boolean',
 ] as const;
 export type TEditableValueType = (typeof editableValueTypes)[number];
 export type TEditableValuesListType = TEditableValueType[];
@@ -11,6 +11,11 @@ export const defaultEditableValueType: TEditableValueType = editableValueTypes[0
 
 export interface TEditableFieldSpec {
   type: TEditableValueType;
-  list?: boolean;
-  validValues?: TEditableValuesListType;
+  // isList?: boolean; // ??? To make list of values (TODO?)
+  // validValues?: TEditableValuesListType; // ???
+  /** Text label used in ui input elements only */
+  label?: string;
 }
+
+export type TEditableValueScalar = string | number | boolean | undefined;
+export type TEditableValue = TEditableValueScalar | TEditableValueScalar[];
--- src/pages/DemoPage/DemoEditableField/DemoEditableField.svelte
+++ src/pages/DemoPage/DemoEditableField/DemoEditableField.svelte
@@ -1,8 +1,29 @@
 <script lang="ts">
+  import { EditableField } from '@/src/components/data';
+  import { TEditableFieldSpec } from '@/src/core/types/editable';
+
   // TODO: Render EditableField
+  const specs: TEditableFieldSpec = {
+    type: 'boolean',
+  };
 </script>
 
-<div class="DemoEditableField">DemoEditableField</div>
+<div class="DemoEditableField">
+  <h2>DemoEditableField</h2>
+  <div>
+    <EditableField
+      specs={{ type: 'boolean', label: 'Test boolean' }}
+      value={false}
+    />
+    <EditableField
+      specs={{ type: 'string', label: 'Test string' }}
+    />
+    <EditableField
+      specs={{ type: 'number', label: 'Test number' }}
+      value={1}
+    />
+  </div>
+</div>
 
 <style lang="scss">
   .DemoEditableField {
