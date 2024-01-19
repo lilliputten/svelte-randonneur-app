58c51516e86cf2c334acdb214c19c50d9b0a8064
lilliputten <lilliputten@yandex.ru>
2024-01-20 00:20:24 +0700

    Issue #38: Minor changes.

--- src/pages/DemoPage/DemoTableWithPagination/DemoTableWithPagination.svelte
+++ src/pages/DemoPage/DemoTableWithPagination/DemoTableWithPagination.svelte
@@ -6,51 +6,14 @@
   import { TGenericEditableData, TGenericEditableSpec } from '@/src/core/types/editable';
   import { TDataSetDictSlot } from '@/src/core/types/rando';
 
-  const dataSetData: TGenericEditableData = [
-    { value: 1 },
-    { value: 2 },
-    { value: 3 },
-    { value: 4 },
-    { value: 5 },
-    { value: 6 },
-    { value: 7 },
-    { value: 8 },
-    { value: 9 },
-    { value: 10 },
-    { value: 11 },
-    { value: 12 },
-    { value: 13 },
-    { value: 14 },
-    { value: 15 },
-    { value: 16 },
-    { value: 17 },
-    { value: 18 },
-    { value: 19 },
-    { value: 20 },
-    { value: 21 },
-    { value: 22 },
-    { value: 23 },
-    { value: 24 },
-    { value: 25 },
-    { value: 26 },
-    { value: 27 },
-    { value: 28 },
-    { value: 29 },
-    { value: 30 },
-    { value: 31 },
-    { value: 32 },
-    { value: 33 },
-    { value: 34 },
-    { value: 35 },
-  ];
+  const itemsCount = 35;
+  const dataSetData: TGenericEditableData = Array.apply(null, Array(itemsCount)).map((_, n) => ({
+    value: n,
+  }));
+
   const dataSetSpec = deriveDataSetSpec('sample', dataSetData as TDataSetDictSlot);
 
   extendDataSetSpec(dataSetSpec);
-
-  console.log('[DemoTableWithPagination]', {
-    dataSetSpec,
-    dataSetData,
-  });
 
   function onChange(data: TGenericEditableData, spec: TGenericEditableSpec) {
     console.log('[DemoTableWithPagination:onChange]', spec.id, {
