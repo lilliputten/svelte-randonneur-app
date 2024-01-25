a32fe491958bf8e5e3de5f1c0d7317ac60b8c4ef
lilliputten <lilliputten@yandex.ru>
2024-01-25 14:21:31 +0700

    Environment bug: added simple test page.

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  // Test page...
+  // import { DemoPage } from '@/src/pages/DemoPage';
+</script>
+
+<div class="Test">
+  Test
+</div>
+
+<style lang="scss">
+  .Test {
+    padding: 20px;
+  }
+</style>
