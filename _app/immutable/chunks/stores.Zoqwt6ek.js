350c06fdd976c2b7a88db205a5f30d4b37fa25fe
lilliputten <lilliputten@yandex.ru>
2024-01-29 20:43:42 +0700

    Build publish.0.0.11, 2024.01.29, 00:27 +0700 (240129-0027)

---
+++ _app/immutable/chunks/stores.Zoqwt6ek.js
@@ -0,0 +1,1 @@
+import{d as e}from"./singletons.WWpI6m4k.js";const r=()=>{const s=e;return{page:{subscribe:s.page.subscribe},navigating:{subscribe:s.navigating.subscribe},updated:s.updated}},b={subscribe(s){return r().page.subscribe(s)}};export{b as p};
