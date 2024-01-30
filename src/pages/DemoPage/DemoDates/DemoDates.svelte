ac142b232dbe16b3f1d011d3b18573d000ba4292
lilliputten <lilliputten@yandex.ru>
2024-01-31 00:02:51 +0700

    Issue 8: Created (in general) a working base for a `DateInput` component (see `DemoDates`).

--- src/pages/DemoPage/DemoDates/DemoDates.svelte
+++ src/pages/DemoPage/DemoDates/DemoDates.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import { DateInput, DatePicker } from 'date-picker-svelte';
   import { Month } from '@svelteuidev/dates';
-  import { Box, Button, Center, Popper } from '@svelteuidev/core';
+  import { ActionIcon, Box, Button, Center, Popper, TextInput } from '@svelteuidev/core';
+  import { Calendar } from 'radix-icons-svelte';
   import dayjs from 'dayjs';
 
   // @see:
@@ -10,34 +11,62 @@
   // - https://www.npmjs.com/package/date-picker-svelte
 
   import { EditableField } from '@/src/components/data';
+  import { isValidDate } from '@/src/core/helpers/basic/dates';
   // import { TEditableFieldSpec } from '@/src/core/types/editable';
 
-  let reference: HTMLAnchorElement | HTMLButtonElement;
-  let mounted = false;
+  // let buttonReference: HTMLAnchorElement | HTMLButtonElement;
+  let inputReference: HTMLInputElement;
+  let calendarOpened = false;
 
-  const toggleMount = () => {
-    mounted = !mounted;
+  const toggleCalendarPopup = () => {
+    // TODO: On open set handlers for esc key press and outside click
+    calendarOpened = !calendarOpened;
   };
 
   const createdDateStrZ = '2023-12-22T01:23:45.67Z';
 
-  let value = new Date(createdDateStrZ);
-  const dateFormat = 'YYYY-MM-DD HH:mm:ss'; // TODO: To use iso format (above)
-  const dayjsDate = dayjs(value);
-  const fmtDate = dayjsDate.format(dateFormat);
-  // eslint-disable-next-line no-console
-  console.log('[DemoDates] Test', {
-    dayjsDate,
-    fmtDate,
-  });
+  let textValue = createdDateStrZ;
+  let value = new Date(textValue);
+
+  /*
+   * const dateFormat = 'YYYY-MM-DD HH:mm:ss'; // TODO: To use iso format (above)
+   * const dayjsDate = dayjs(value);
+   * const fmtDate = dayjsDate.format(dateFormat);
+   * // eslint-disable-next-line no-console
+   * console.log('[DemoDates] Test', {
+   *   dayjsDate,
+   *   fmtDate,
+   * });
+   */
 
   function selectDate(ev: CustomEvent<Date>) {
     const date = ev.detail;
+    value = date;
+    textValue = date.toISOString();
     console.log('selectDate', {
+      value,
+      textValue,
       date,
       ev,
     });
-    value = date;
+  }
+
+  function handleTextDate(ev: CustomEvent<string>) {
+    const input = ev.currentTarget as HTMLInputElement;
+    const text = input?.value;
+    // const text = ev.detail;
+    textValue = text;
+    const newDate = new Date(text);
+    if (isValidDate(newDate)) {
+      value = newDate;
+    }
+    console.log('selectDate', {
+      value,
+      textValue,
+      text,
+      ev,
+    });
+    debugger;
   }
 </script>
 
@@ -60,11 +89,20 @@
     -->
 
     <h2>Button & Popper:</h2>
-    <Button bind:element={reference} on:click={toggleMount}>{value.toISOString()}</Button>
+    <!--
+    <Button bind:element={buttonReference} on:click={toggleCalendarPopup}>{value.toISOString()}</Button>
+    -->
+    <TextInput bind:element={inputReference} value={value.toISOString()} on:change={handleTextDate}>
+      <svelte:fragment slot="rightSection">
+        <ActionIcon on:click={toggleCalendarPopup} title="Toggle date selector">
+          <Calendar />
+        </ActionIcon>
+      </svelte:fragment>
+    </TextInput>
     <Popper
       title="Select date"
-      {mounted}
-      {reference}
+      mounted={calendarOpened}
+      reference={inputReference}
       position="bottom"
       placement="start"
       gutter={4}
